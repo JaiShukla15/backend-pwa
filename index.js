1c063168e56c6f3cb0b9c13e217c21745ec802d4
jai shukla <shuklajay012@gmail.com>
2023-07-27 14:45:59 +0530

    changes for temp

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const session = require("express-session");
 const swaggerJSON = require('./Swagger.json');
 const ErrorHandler = require("./middlewares/errorHandler");
 var SequelizeStore = require("connect-session-sequelize")(session.Store);
+// const webPush = require('web-push');
 app.use(express.json());
 app.use(
   session({
@@ -22,19 +23,18 @@ app.use(
   })
 );
 app.use(cors());
-const webPush = require('web-push');
 
 
 // console.log(webPush.generateVAPIDKeys());
 
 
-const publicKey = 'BN69bTRuil6wT1gdV_daOX6usqgVZofjaFUDFz-2WtKeC2YlUGEMz8vvVbqhuA-cp8IwBFfrde6Dp3QhiEwgyfc';
-const privateKey = 'mLVmbk81MabnJN3UETnwbV-4LdMsSEmqVey7QqRFogI';
+// const publicKey = 'BN69bTRuil6wT1gdV_daOX6usqgVZofjaFUDFz-2WtKeC2YlUGEMz8vvVbqhuA-cp8IwBFfrde6Dp3QhiEwgyfc';
+// const privateKey = 'mLVmbk81MabnJN3UETnwbV-4LdMsSEmqVey7QqRFogI';
 
 
-const sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/dxW1nTP8cp8:APA91bFk75BXSjh5WB6FCJjhiiuDd8CFgD5am8bZRuzmdSm0Cgr48QfRqogD037l2cNa2OjIt-QgOyOB0-gKBKppWHT5JDe5Iaj1pPFxTFQUwGKJLkuQMUwwQp1XUG_4qA5FFJ_TW3oi","expirationTime":null,"keys":{"p256dh":"BKXWSFDWvuLIL4aBuIVSue2rLhCUPFBE8KiQm3aqYudg15pagMo6simLk6DDONQXAGr-YGgd_pS9ecZ9deipzWg","auth":"rEUZnC9TCCFEMgLYARo7xg"}}
+// const sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/dxW1nTP8cp8:APA91bFk75BXSjh5WB6FCJjhiiuDd8CFgD5am8bZRuzmdSm0Cgr48QfRqogD037l2cNa2OjIt-QgOyOB0-gKBKppWHT5JDe5Iaj1pPFxTFQUwGKJLkuQMUwwQp1XUG_4qA5FFJ_TW3oi","expirationTime":null,"keys":{"p256dh":"BKXWSFDWvuLIL4aBuIVSue2rLhCUPFBE8KiQm3aqYudg15pagMo6simLk6DDONQXAGr-YGgd_pS9ecZ9deipzWg","auth":"rEUZnC9TCCFEMgLYARo7xg"}}
 
-webPush.setVapidDetails('mailto:example@yopmail.com', publicKey, privateKey);
+// webPush.setVapidDetails('mailto:example@yopmail.com', publicKey, privateKey);
 
 
 app.get('/api/notification',(req,res)=>{
@@ -45,7 +45,7 @@ app.get('/api/notification',(req,res)=>{
       "vibrate":[100,50,100]
     }
   }
-  webPush.sendNotification(sub,JSON.stringify(payload));
+  // webPush.sendNotification(sub,JSON.stringify(payload));
    res.send('<h1>Notification sent !</h1>')
 });
 
